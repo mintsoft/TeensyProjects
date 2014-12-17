22ea368dd00b0082973da6845611ff3d2ab25c51
Rob Emery <rob@mintsoft.net>
2014-12-17 22:28:28 +0000

    Indentation fixes

--- BigRedButton.ino
+++ BigRedButton.ino
@@ -2,45 +2,45 @@ void performAction(int index)
 {
   switch(index){
     case 0:
-    Keyboard.set_key1(KEY_ENTER);
+      Keyboard.set_key1(KEY_ENTER);
     break;
     case 1:
-    Keyboard.set_key1(KEY_F5);
+      Keyboard.set_key1(KEY_F5);
     break;
     case 2:
-    Keyboard.set_modifier(MODIFIERKEY_CTRL);
-    Keyboard.set_key1(KEY_C);
+      Keyboard.set_modifier(MODIFIERKEY_CTRL);
+      Keyboard.set_key1(KEY_C);
     break;
     case 3:
-    Keyboard.set_modifier(MODIFIERKEY_ALT);
-    Keyboard.set_key1(KEY_F4);
+      Keyboard.set_modifier(MODIFIERKEY_ALT);
+      Keyboard.set_key1(KEY_F4);
     break;
     case 4:
-    Keyboard.set_modifier(MODIFIERKEY_CTRL|MODIFIERKEY_SHIFT);
-    Keyboard.set_key1(KEY_ESC);
+      Keyboard.set_modifier(MODIFIERKEY_CTRL|MODIFIERKEY_SHIFT);
+      Keyboard.set_key1(KEY_ESC);
     break;
     case 5:
-    Keyboard.set_modifier(MODIFIERKEY_CTRL|MODIFIERKEY_ALT);
-    Keyboard.set_key1(KEY_DELETE);
+      Keyboard.set_modifier(MODIFIERKEY_CTRL|MODIFIERKEY_ALT);
+      Keyboard.set_key1(KEY_DELETE);
     break;
     case 6:
-    Keyboard.set_modifier(MODIFIERKEY_CTRL);
-    Keyboard.set_key1(KEY_A);
+      Keyboard.set_modifier(MODIFIERKEY_CTRL);
+      Keyboard.set_key1(KEY_A);
     break;
     case 7:
-    Keyboard.set_key1(KEY_ESC);
+      Keyboard.set_key1(KEY_ESC);
     break;
     case 8:
-    Keyboard.set_key1(KEY_DELETE);
+      Keyboard.set_key1(KEY_DELETE);
     break;
     case 9:
-    Keyboard.set_key1(KEY_SPACE);
+      Keyboard.set_key1(KEY_SPACE);
     break;
     case 10:
-    Keyboard.set_key1(KEY_Y);
+      Keyboard.set_key1(KEY_Y);
     break;
     case 11:
-    Keyboard.set_key1(KEY_N);
+      Keyboard.set_key1(KEY_N);
     break;
   }
   Keyboard.send_now();
