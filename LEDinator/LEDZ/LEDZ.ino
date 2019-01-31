7a05f4949c6785f9999a1a43ecd59b9a10a7377c
Rob Emery <mintsoft@users.noreply.github.com>
2019-01-31 22:41:17 +0000

    Update LEDZ.ino
    Gutted down to just the LED handling

--- LEDinator/LEDZ/LEDZ.ino
+++ LEDinator/LEDZ/LEDZ.ino
@@ -1,4 +1,7 @@
 
+#define NO_TIMEOUT 0
+byte buffer[64];
+
 void setup() {
   Serial.begin(9600);
   Serial.println(F("RawHID Example"));
@@ -7,52 +10,17 @@ void setup() {
   }
 }
 
-// RawHID packets are always 64 bytes
-byte buffer[64];
-elapsedMillis msUntilNextSend;
-unsigned int packetCount = 0;
-#define NO_TIMEOUT 0
 void loop() {
   int n;
+  
   n = RawHID.recv(buffer, NO_TIMEOUT);
+
   if (n > 0) {
-    // the computer sent a message.  Display the bits
-    // of the first byte on pin 0 to 7.  Ignore the
-    // other 63 bytes!
     Serial.print(F("Received packet, first byte: "));
     Serial.println((int)buffer[0]);
     for (int i=0; i<8; i++) {
       int b = buffer[0] & (1 << i);
       digitalWrite(i, b);
-    }
-  }
-  // every 2 seconds, send a packet to the computer
-  if (msUntilNextSend > 2000) {
-    msUntilNextSend = msUntilNextSend - 2000;
-    // first 2 bytes are a signature
-    buffer[0] = 0xAB;
-    buffer[1] = 0xCD;
-    // next 24 bytes are analog measurements
-    for (int i=0; i<12; i++) {
-      int val = analogRead(i);
-      buffer[i * 2 + 2] = highByte(val);
-      buffer[i * 2 + 3] = lowByte(val);
-    }
-    // fill the rest with zeros
-    for (int i=26; i<62; i++) {
-      buffer[i] = 0;
-    }
-    // and put a count of packets sent at the end
-    buffer[62] = highByte(packetCount);
-    buffer[63] = lowByte(packetCount);
-    // actually send the packet
-    n = RawHID.send(buffer, 100);
-    if (n > 0) {
-      Serial.print(F("Transmit packet "));
-      Serial.println(packetCount);
-      packetCount = packetCount + 1;
-    } else {
-      Serial.println(F("Unable to transmit packet"));
     }
   }
 }
