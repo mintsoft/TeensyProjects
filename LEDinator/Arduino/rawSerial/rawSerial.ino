5c89b22e23040f9740e0e0c57450a21f447263f2
Rob Emery <mintsoft@users.noreply.github.com>
2019-02-02 21:36:22 +0000

    need to ignore whitespace and allsorts for this to work with an echo x > com7 command.
    Adding debugging buffer to help with why it doesn't work under raw linux

--- LEDinator/Arduino/rawSerial/rawSerial.ino
+++ LEDinator/Arduino/rawSerial/rawSerial.ino
@@ -1,27 +1,46 @@
+#define ASCII_DIGIT_OFFSET 0x30;
+
+char buffer[255];
+int y;
 
 void setup() {
   Serial.begin(9600);
-  for (int i=0; i<12; i++) {
+  for (int i=0; i<10; i++) {
     pinMode(i, OUTPUT);
     digitalWrite(i, HIGH);
   }
+  y = 0;
 }
+
+//in cmd::  mode com7 BAUD=9600 PARITY=n DATA=8 STOP=1 && echo 3 > com7
 
 void loop() {
   char received_char;
   int pin_number;
-
+  
   while( Serial.available() > 0 ) {
     received_char = Serial.read();
 
     //Serial.print("Received Something: ");
     //Serial.println(received_char);
+    buffer[y%255] = received_char;
+    y++;
     
-    if(received_char < 0) {
+    pin_number = received_char - ASCII_DIGIT_OFFSET;
+    
+    if(pin_number < 0 || pin_number > 9) {
       continue;
     }
     
-    pin_number = received_char - 0x30;
+    if(pin_number == 9) {
+      Serial.println("I have seen: ");
+      for(int x=0; x<255; ++x) {
+        Serial.print("'");
+        Serial.print(buffer[x]);
+        Serial.println("'");
+      }
+      Serial.println("I'm done");
+    }
     for (int i=0; i<10; i++) {
       if(i == pin_number) {
         //Serial.print("Setting low pin: ");
